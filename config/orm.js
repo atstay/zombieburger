e032a6506d5930425c3367d5e5c0ed0da0a56935
Shubalub Kingz <atstay@gmail.com>
2018-12-13 11:34:40 -0700

    orm update

--- config/orm.js
+++ config/orm.js
@@ -1,51 +1,52 @@
 // Node Dependencies
-var connection = require('./connection.js');
+var connection = require("./connection.js");
+
+// Connect to MySQL database
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 // MySQL commands
 var orm = {
-
   // selectAll()
   selectAll: function(cb) {
-
     // Run MySQL Query
-    connection.query('SELECT * FROM burgers', function (err, result) {
+    connection.query("SELECT * FROM burgers", function(err, result) {
       if (err) throw err;
       cb(result);
     });
-
   },
 
   // insertOne()
-  insertOne: function(burger_name, cb){
-			connection.query('INSERT INTO burgers SET ?', 
-				{	burger_name: burger_name,
-					devoured: false,
-				}, function(err, result)
-				{
-					if (err) throw err;
-					console.log (error);
-					cb(result);
-				});
-					
-		},
+  insertOne: function(burger_name, cb) {
+    connection.query(
+      "INSERT INTO burgers SET ?",
+      { burger_name: burger_name, devoured: false },
+      function(err, result) {
+        if (err) throw err;
+        console.log(error);
+        cb(result);
+      }
+    );
+  },
 
   // updateOne()
-  updateOne: function(burgerID, cb){
-
+  updateOne: function(burgerID, cb) {
     // Run MySQL Query
-		connection.query('UPDATE burgers SET ? WHERE ?', 
-		[{devoured: true}, 
-			{id: burgerID}], 
-			function (err, result) {
+    connection.query(
+      "UPDATE burgers SET ? WHERE ?",
+      [{ devoured: true }, { id: burgerID }],
+      function(err, result) {
         if (err) throw err;
         cb(result);
-      });
-
+      }
+    );
   }
-
 };
-
-
 
 // Export the ORM object in module.exports.
 module.exports = orm;
