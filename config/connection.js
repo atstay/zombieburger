11cf8e90a5419d8518771578d06834ca256fd045
Shubalub Kingz <atstay@gmail.com>
2018-12-13 11:17:10 -0700

    work please

--- config/connection.js
+++ config/connection.js
@@ -2,22 +2,23 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "us-cdbr-iron-east-01.cleardb.net",
   port: 3306,
-  user: "root",
-  password: "denveruniversity",
-  database: "burgers_db"
+  user: "ba847175ed5197",
+  password: "1f28d958",
+  database: "heroku_593520653950a97"
 });
+
+//ba847175ed5197:1f28d958@us-cdbr-iron-east-01.cleardb.net/heroku_593520653950a97?reconnect=true
 
 // Connect to MySQL database
 connection.connect(function(err) {
   if (err) {
-    console.error('error connecting: ' + err.stack);
+    console.error("error connecting: " + err.stack);
     return;
-  };
-  console.log('connected as id ' + connection.threadId);
+  }
+  console.log("connected as id " + connection.threadId);
 });
-
 
 // Export connection for our ORM to use.
 module.exports = connection;
